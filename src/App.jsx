4b8032bf58d9f99eb981974858ae523842cb337e
danifari <dani.farinola@gmail.com>
2026-07-29 13:07:20 +0200

    background set

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
 import Header from './components/Header';
 import Home from './components/Home';
 import Icons from './components/Icons';
+import Grainient from './Grainient';
 
 function App() {
   return(
     <>
     <Header></Header>
     <Home></Home>
+    <div style={{ position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          zIndex: -1, }}>
+      <Grainient
+        color1="#000000"
+        color2="#181819"
+        color3="#EC4899"
+        timeSpeed={3.95}
+        colorBalance={0.37}
+        warpStrength={1}
+        warpFrequency={6.9}
+        warpSpeed={2}
+        warpAmplitude={60}
+        blendAngle={0}
+        blendSoftness={0.05}
+        rotationAmount={500}
+        noiseScale={2}
+        grainAmount={0.1}
+        grainScale={2}
+        grainAnimated={false}
+        contrast={1.5}
+        gamma={1}
+        saturation={1}
+        centerX={0}
+        centerY={0}
+        zoom={0.9}
+      />
+    </div>
     <Icons></Icons>
     </>
   );
