5acc66ed80e81bb34dd95d787dd8bff58ba04886
danifari <dani.farinola@gmail.com>
2026-07-29 20:29:47 +0200

    animated Title

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,14 @@ function Home() {
     <>
       <div className="container">
         <div className="img-container"></div>
-        <h1 className="maintitle">Swaggin since I smash yo mama</h1>
+        <h1 class="maintitle">
+            <span class="word">SWAGGING </span>
+            <span class="word"> SINCE </span>
+            <span class="word"> I </span>
+            <span class="word"> SMASH </span>
+            <span class="word"> YO </span>
+            <span class="word"> MAMA </span>
+        </h1>
       </div>
       <p>MP4 non è solo un party, è un'esperienza sonora. Nato dall'esigenza di portare la vera attitudine del Rap e della Trap d'oltreoceano nei club, il collettivo ridefinisce le regole della notte. Con la direzione artistica e i suoni di Matthew Wrld & Difo, e spinto dalla vision di 2999, MP4 trasforma ogni set in un rituale immersivo. Dai bassi profondi dell'Hip Hop americano fino ai ritmi frenetici del Baile Funk e ai Club Edits più esclusivi, portiamo un'energia cruda e senza filtri direttamente in pista. From 23:00 till late: we run the club..</p>
     </>
