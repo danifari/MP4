9b264b551f079be9ce12019c43f90602d01d627c
danifari <dani.farinola@gmail.com>
2026-07-30 13:08:25 +0200

    static ipod

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,16 +6,39 @@ function Home() {
     <>
       <div className="container">
         <div className="img-container"></div>
-        <h1 class="maintitle">
-            <span class="word">SWAGGING </span>
-            <span class="word"> SINCE </span>
-            <span class="word"> I </span>
-            <span class="word"> SMASH </span>
-            <span class="word"> YO </span>
-            <span class="word"> MAMA </span>
+        <h1 className="maintitle">
+            <span className="word">LOREM </span>
+            <span className="word"> IPSUM </span>
+            <span className="word"> DOLOR </span>
+            <span className="word"> SIT </span>
+            <span className="word"> AMET </span>
+            <span className="word"> CONSECTETUR </span>
         </h1>
       </div>
       <p>MP4 non è solo un party, è un'esperienza sonora. Nato dall'esigenza di portare la vera attitudine del Rap e della Trap d'oltreoceano nei club, il collettivo ridefinisce le regole della notte. Con la direzione artistica e i suoni di Matthew Wrld & Difo, e spinto dalla vision di 2999, MP4 trasforma ogni set in un rituale immersivo. Dai bassi profondi dell'Hip Hop americano fino ai ritmi frenetici del Baile Funk e ai Club Edits più esclusivi, portiamo un'energia cruda e senza filtri direttamente in pista. From 23:00 till late: we run the club..</p>
+      <div className="discover">
+        <h2 className='genreh'>DISCOVER THE GENRE WE PLAY </h2>
+        <div className="ipod">
+          <div className="screen">
+            <div className="content">
+              <h2>MP4</h2>
+              <p>Experience the sound of the night</p>
+            </div>
+          </div>
+          <div className="buttons">
+            {/* Questo è il cerchio centrale vuoto e scuro */}
+            <div className="center-button"></div>
+            
+            {/* I bottoni verticali esistenti (Top/Bottom) */}
+            <button className="play-button">Play</button>
+            <button className="pause-button">Pause</button>
+
+            {/* NUOVI: I bottoni orizzontali (Left/Right) per cambiare traccia */}
+            <button className="prev-track-button">←</button>
+            <button className="next-track-button">→</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
